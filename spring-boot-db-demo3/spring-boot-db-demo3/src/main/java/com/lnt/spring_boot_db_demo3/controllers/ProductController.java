0cabab09fc880fa19e7baf3e32123911ed060474
Puneet Vashisht <puneetvashisht@gmail.com>
2024-06-21 11:55:03 +0530

    logging

--- spring-boot-db-demo3/spring-boot-db-demo3/src/main/java/com/lnt/spring_boot_db_demo3/controllers/ProductController.java
+++ spring-boot-db-demo3/spring-boot-db-demo3/src/main/java/com/lnt/spring_boot_db_demo3/controllers/ProductController.java
@@ -5,16 +5,41 @@ import org.springframework.web.bind.annotation.RestController;
 import com.lnt.spring_boot_db_demo3.entities.Product;
 import com.lnt.spring_boot_db_demo3.repos.ProductRepository;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
 @RestController
 @RequestMapping("/api/v1")
+@Slf4j
 public class ProductController {
+// Logger log = LoggerFactory.getLogger(ProductController.class);
+
+
+
+    @RequestMapping("/")
+    public String index() {
+       log.trace("A TRACE Message");
+       log.debug("A DEBUG Message");
+       log.info("An INFO Message");
+       log.warn("A WARN Message");
+       log.error("An ERROR Message");
+
+        return "Howdy! Check out the Logs to see the output...";
+    }
+
     @Autowired
     ProductRepository productRepository;
 
@@ -22,5 +47,13 @@ public class ProductController {
     public void addProduct(@RequestBody Product product){
         productRepository.save(product);
     }
+
+    @GetMapping("/products")
+    public List<Product> fetchProducts() {
+        List<Product> products = productRepository.findAll();
+        log.debug(products.toString());
+        return products;
+    }
+    
     
 }
--- spring-boot-db-demo3/spring-boot-db-demo3/src/main/java/com/lnt/spring_boot_db_demo3/entities/Product.java
+++ spring-boot-db-demo3/spring-boot-db-demo3/src/main/java/com/lnt/spring_boot_db_demo3/entities/Product.java
@@ -4,36 +4,38 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 @Entity
+@Data
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     int id;
     String name;
     double price;
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public double getPrice() {
-        return price;
-    }
-    public void setPrice(double price) {
-        this.price = price;
-    }
-    @Override
-    public String toString() {
-        return "Product [id=" + id + ", name=" + name + ", price=" + price + "]";
-    }
+    // public int getId() {
+    //     return id;
+    // }
+    // public void setId(int id) {
+    //     this.id = id;
+    // }
+    // public String getName() {
+    //     return name;
+    // }
+    // public void setName(String name) {
+    //     this.name = name;
+    // }
+    // public double getPrice() {
+    //     return price;
+    // }
+    // public void setPrice(double price) {
+    //     this.price = price;
+    // }
+    // @Override
+    // public String toString() {
+    //     return "Product [id=" + id + ", name=" + name + ", price=" + price + "]";
+    // }
 
 
     
